b896f01219dfddd5d18a49dd1197b72f3721717c
Eric McDaniel <mcdanieles@gmail.com>
2025-08-04 22:46:15 -0500

    Setup security for cicd

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # LumenLab
 
-### Turning pixels into a playground — because lighting should be more than just bright
+### Transforming pixels into a playground — because lighting should be more than just bright
+
+# Build
+In order to simplify the build process for this repo, a script to initialize global configuration settings and credentials has been provided for both Windows and *nix OS. These scripts auto-generate the relevant files needed for a clean build.
+
+|File|Description|
+|---|---|
+|secrets.h|A header file that #define variables at compile-time, such as your WLAN credentials, and host/port ip address|
+|platformio-override.ini|Environment settings for using PlatformIO, such as your baud rate and upload_port|
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,67 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include "secrets.h"
+
+const char *ssid = WIFI_SSID;
+const char *password = WIFI_PASSWORD;
+
+const char *clientIpAddress = CLIENT_IP_ADDRESS;
+const int clientPort = CLIENT_PORT;
+
+uint8_t ledBuffer[300 * 3] = {0};
+
+WiFiUDP udp;
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial.println("Starting esp32 up.");
+  Serial.begin(SERIAL_BAUD);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConnected to WiFi");
   pinMode(BUILTIN_LED, OUTPUT);
 }
+
+int count = 0;
 
 void loop()
 {
   digitalWrite(BUILTIN_LED, HIGH);
   Serial.println("On.");
-  delay(100);
+  delay(250);
 
   digitalWrite(BUILTIN_LED, LOW);
   Serial.println("Off.");
-  delay(100);
+
+  for (int i = 0; i < 300; i++)
+  {
+    int base = i * 3;
+    ledBuffer[base + 0] = count % 3 == 0 ? 255 : 0;
+    ledBuffer[base + 1] = count % 3 == 1 ? 255 : 0;
+    ledBuffer[base + 2] = count % 3 == 2 ? 255 : 0;
+  }
+  count++;
+
+  Serial.printf("Sending %d bytes to %s:%d\n", sizeof(ledBuffer), clientIpAddress, clientPort);
+
+  if (udp.beginPacket(clientIpAddress, clientPort))
+  {
+    int written = udp.write(ledBuffer, sizeof(ledBuffer));
+    udp.endPacket();
+    Serial.printf("Wrote %d bytes\n", written);
+  }
+  else
+  {
+    Serial.println("Failed to begin UDP packet");
+  }
+
+  Serial.println("UDP packet sent");
+
+  delay(200);
 }

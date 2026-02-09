1c565121b0a18c25eb357d2fd1869408e9c2b548
Eric McDaniel <mcdanieles@gmail.com>
2026-02-08 22:04:29 -0600

    Adjust spacing, refactor

--- src/display/menu-navigation.cpp
+++ src/display/menu-navigation.cpp
@@ -7,10 +7,11 @@ namespace Display
   void MenuTileNavigation::displayMenuNavigation()
   {
     constexpr uint8_t numModes = static_cast<uint8_t>(Engine::MainMenuSelection::COUNT);
-    uint8_t modeSelected = static_cast<uint8_t>(contextManager->stateManager.getUserMenuChoice());
+    Engine::MainMenuSelection modeSelected = contextManager->stateManager.getUserMenuChoice();
+    Engine::SystemState currentState = contextManager->stateManager.current();
 
     uint16_t displayIndex = SystemCore::Configuration::numLeds - 1;
-    float inactiveSelectionDimmingScale = contextManager->stateManager.current() == Engine::SystemState::MenuHome ? 1.0f : 0.3f;
+    float inactiveSelectionDimmingScale = currentState == Engine::SystemState::MenuHome ? 1.0f : 0.3f;
     constexpr float center = (menuTileWidth - 1) / 2.0f;
     constexpr double sigma = 3.0f;
 
@@ -21,7 +22,7 @@ namespace Display
         float x = i - center;
         float blend = std::exp(-(x * x) / (2 * sigma * sigma)); // computed gaussian curve
 
-        if (modeIdx == modeSelected)
+        if (modeIdx == static_cast<uint8_t>(modeSelected))
           contextManager->leds.buffer[displayIndex] = Lights::Color{Lights::ColorCode::ThemeGreen} * inactiveSelectionDimmingScale * blend;
         else
           contextManager->leds.buffer[displayIndex] = Lights::Color{Lights::ColorCode::MenuUnselected} * inactiveSelectionDimmingScale * blend;
@@ -30,7 +31,7 @@ namespace Display
       }
 
       if (modeIdx < numModes - 1)
-        displayIndex -= menuTileWidth;
+        displayIndex -= (menuTileWidth / 2);
     }
 
     constexpr uint8_t numGames = static_cast<uint8_t>(Engine::GameSelection::COUNT);
@@ -40,8 +41,8 @@ namespace Display
     uint8_t sceneSelected = static_cast<uint8_t>(contextManager->stateManager.getUserSceneChoice());
 
     displayIndex = 0;
-    inactiveSelectionDimmingScale = (contextManager->stateManager.current() == Engine::SystemState::MenuGames || contextManager->stateManager.current() == Engine::SystemState::MenuScenes) ? 1.0f : 0.3f;
-    uint8_t gamesOrScenesAvailable = contextManager->stateManager.getUserMenuChoice() == Engine::MainMenuSelection::Games ? numGames : numScenes;
+    inactiveSelectionDimmingScale = (currentState == Engine::SystemState::MenuGames || currentState == Engine::SystemState::MenuScenes) ? 1.0f : 0.3f;
+    uint8_t gamesOrScenesAvailable = modeSelected == Engine::MainMenuSelection::Games ? numGames : numScenes;
 
     for (uint8_t modeIdx = 0; modeIdx < gamesOrScenesAvailable; ++modeIdx)
     {
@@ -50,9 +51,9 @@ namespace Display
         float x = i - center;
         float blend = std::exp(-(x * x) / (2 * sigma * sigma));
 
-        if (contextManager->stateManager.current() == Engine::SystemState::MenuGames && modeIdx == gameSelected)
+        if (currentState == Engine::SystemState::MenuGames && modeIdx == gameSelected)
           contextManager->leds.buffer[displayIndex] = Lights::Color{Lights::ColorCode::ThemeBlue} * blend;
-        else if (contextManager->stateManager.current() == Engine::SystemState::MenuScenes && modeIdx == sceneSelected)
+        else if (currentState == Engine::SystemState::MenuScenes && modeIdx == sceneSelected)
           contextManager->leds.buffer[displayIndex] = Lights::Color{Lights::ColorCode::ThemeYellow} * blend;
         else
           contextManager->leds.buffer[displayIndex] = Lights::Color{Lights::ColorCode::MenuUnselected} * inactiveSelectionDimmingScale * blend;
@@ -61,7 +62,7 @@ namespace Display
       }
 
       if (modeIdx < gamesOrScenesAvailable - 1)
-        displayIndex += menuTileWidth;
+        displayIndex += (menuTileWidth / 2);
     }
   }
 }

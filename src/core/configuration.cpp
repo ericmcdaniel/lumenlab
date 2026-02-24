049f9b97bad581f1a0cf58677df2e01619a74205
Eric McDaniel <mcdanieles@gmail.com>
2026-02-23 19:23:09 -0600

    Combine embedded bindings with NVS memory

--- src/core/configuration.cpp
+++ src/core/configuration.cpp
@@ -1,6 +1,10 @@
 #include "core/configuration.h"
 #include <Preferences.h>
 
+#ifndef LUMENLAB_VERSION
+#define LUMENLAB_VERSION "v999.999.999"
+#endif
+
 namespace SystemCore
 {
   String Configuration::_version;
@@ -11,7 +15,7 @@ namespace SystemCore
 
   void Configuration::load(::Preferences &memory)
   {
-    _version = memory.getString("version", "v0.0.0");
+    _version = LUMENLAB_VERSION;
     _macAddress = memory.getString("macAddress", "00:00:00:00:00:00");
     _numLeds = static_cast<uint16_t>(memory.getString("numLeds", "300").toInt());
     _serialBaud = static_cast<uint32_t>(memory.getString("serialBaud", "921600").toInt());

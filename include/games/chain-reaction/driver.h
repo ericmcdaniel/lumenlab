13c58d69a6c315de95a254623bd24d49294aa62c
Eric McDaniel <mcdanieles@gmail.com>
2026-03-14 23:43:17 -0500

    Start module for Chain Reaction

---
+++ include/games/chain-reaction/driver.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "engine/layer.h"
+#include "engine/timer.h"
+#include "core/context-manager.h"
+
+namespace Games::ChainReaction
+{
+  class Driver : public Engine::Layer, private Engine::Timer
+  {
+  public:
+    Driver(SystemCore::ContextManager *ctx) {}
+
+  private:
+    SystemCore::ContextManager *contextManager;
+    // GameState &state;
+  };
+}
